c3b928919d494534fd9d49698bd59e362fcc22ab
Andrew Sakhnovskyi <samuelson1187@gmail.com>
2019-09-14 17:29:41 +0300

    second

--- app/js/common.js
+++ app/js/common.js
@@ -5,6 +5,17 @@ $(window).on('load', function() {
 	$('.preloader').delay(2000).fadeOut('slow');
 });
 
+// Menu mobile
+$('.header__nav--open').click(function (e) {
+    e.preventDefault();
+	$('.header__mobile').toggleClass('header__mobile--visible');
+});
+
+$('.header__nav-item').click(function(e){
+	e.preventDefault();
+	$(this).closest('.header__mobile').removeClass('header__mobile--visible');
+});
+
 // Header scroll
 $(window).on('scroll', function(){
 	if ($(window).scrollTop()){
